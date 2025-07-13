6d14bfb74a188868d43ea59b0fb57289adc5938b
Akshaya <itz.akshaya001@gmail.com>
2025-07-13 11:50:51 +0530

    Delete hooks/lib directory

--- hooks/lib/contract.ts
+++
@@ -1,5 +0,0 @@
-// File: lib/contract.ts
-import ProjectLoggerArtifact from "../contracts/artifacts/contracts/ProjectLogger.sol/ProjectLogger.json";
-
-export const CONTRACT_ABI = ProjectLoggerArtifact.abi;
-export const CONTRACT_ADDRESS = "0xE474A6F728F8eE2D22a686eC52135d21b7AD99Bd";
